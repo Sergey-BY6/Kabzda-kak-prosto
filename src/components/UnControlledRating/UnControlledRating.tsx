e6455a606b697bc4fe4ca024f541fe00e5c16d75
Sergey Babich <sergei.babich2022@yandex.by>
2023-01-21 15:05:37 +0300

    Solution of the task.

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import {MouseEvent} from 'react'
 
 
 
@@ -19,11 +19,16 @@ export function UnControlledRating(props: RatingPropsType) {
 
     return (
         <div>
-            <Star selected={value > 0}/><button onClick={() => changedValue(1)}>1</button>
-            <Star selected={value > 1}/><button onClick={() => changedValue(2)}>2</button>
-            <Star selected={value > 2}/><button onClick={() => changedValue(3)}>3</button>
-            <Star selected={value > 3}/><button onClick={() => changedValue(4)}>4</button>
-            <Star selected={value > 4}/><button onClick={() => changedValue(5)}>5</button>
+            {/*<Star selected={value > 0}/><button onClick={() => changedValue(1)}>1</button>*/}
+            {/*<Star selected={value > 1}/><button onClick={() => changedValue(2)}>2</button>*/}
+            {/*<Star selected={value > 2}/><button onClick={() => changedValue(3)}>3</button>*/}
+            {/*<Star selected={value > 3}/><button onClick={() => changedValue(4)}>4</button>*/}
+            {/*<Star selected={value > 4}/><button onClick={() => changedValue(5)}>5</button>*/}
+            <Star selected={value > 0} starId={1} callback={changedValue}/>
+            <Star selected={value > 1} starId={2} callback={changedValue}/>
+            <Star selected={value > 2} starId={3} callback={changedValue}/>
+            <Star selected={value > 3} starId={4} callback={changedValue}/>
+            <Star selected={value > 4} starId={5} callback={changedValue}/>
         </div>
     )
 }
@@ -32,14 +37,23 @@ export function UnControlledRating(props: RatingPropsType) {
 
 type StarPropsType = {
     selected: boolean
-
+    starId: ValueType
+    callback: (values: ValueType) => void
 }
 
 function Star(props: StarPropsType) {
 
+
+    const starValue = () => {
+        console.log(props.starId)
+        props.callback(props.starId)
+    }
+
+
+
     return props.selected ?
-        <span><b>star</b></span> :
-        <span>star</span>
+        <span onClick={starValue}><b>star</b></span> :
+        <span onClick={starValue}>star</span>
 
     // if (props.selected) {
     //     return (
