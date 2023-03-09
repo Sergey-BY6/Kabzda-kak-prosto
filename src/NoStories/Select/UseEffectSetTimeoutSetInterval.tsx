4f02fae6d57a558d2b7bc619a62f17b53b571c63
Sergey Babich <sergei.babich2022@yandex.by>
2023-03-09 21:51:57 +0300

    Add setTimeout, setInterval in useEffect, homework, lessons 22.

--- src/NoStories/Select/UseEffectSetTimeoutSetInterval.tsx
+++ src/NoStories/Select/UseEffectSetTimeoutSetInterval.tsx
@@ -31,23 +31,42 @@ export const UseEffectSetInterval = () => {
 
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
+    const [time, setTime] = useState(new Date())
 
     console.log("UseEffectSetInterval")
     console.log(counter)
 
-    useEffect (()=> {
-        setInterval (()=> {
-            console.log("tik" + counter)
-            setCounter((state) => state + 1)
-        }, 1000)
+    // useEffect (()=> {
+    //     setInterval (()=> {
+    //         console.log("tik" + counter)
+    //         setCounter((state) => state + 1)
+    //     }, 1000)
+    // },[])
+
+
+    // return (
+    //     <div>
+    //         Hello,counter: {counter} - fake: {fake}
+    //         {/*<button onClick={() => setCounter(counter + 1)}>counter +</button>*/}
+    //         {/*<button onClick={() => setFake(fake + 1)}>fake +</button>*/}
+    //     </div>
+    // )
+
+
+    useEffect(()=> {
+        setInterval (()=>{
+            setTime(new Date())
+        },1000)
     },[])
+
+    let times = new Intl.DateTimeFormat ("ru", {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    }).format(time)
 
 
     return (
-        <div>
-            Hello,counter: {counter} - fake: {fake}
-            {/*<button onClick={() => setCounter(counter + 1)}>counter +</button>*/}
-            {/*<button onClick={() => setFake(fake + 1)}>fake +</button>*/}
-        </div>
+        <div>{times}</div>
     )
 }
